aa541c3438a21f1fbdc7b806e4349133db862795
azeemzeeshann <azeemzeeshann@gmail.com>
2025-07-29 09:12:55 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JavaScript Example</title>
+</head>
+<body>
+
+  <h1>Click the Button</h1>
+  <button onclick="showMessage()">Click Me</button>
+
+  <script>
+    function showMessage() {
+      alert("Hello! You clicked the button.");
+    }
+  </script>
+
+</body>
+</html>
